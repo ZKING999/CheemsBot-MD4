9bc934e73fffd0b9c6330ad8e1ef76866267dc5a
ZKING999 <103749843+ZKING999@users.noreply.github.com>
2022-12-23 10:27:38 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,8 +54,10 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.vcardowner = ['923030746588'] //ur owner number
+global.ownername = "🦄ZKINGMODSZKINGMODS
+
+" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
